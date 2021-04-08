9577e4f5ac5fe583abcd060b9537d690df427d84
Anna <anna@joomla.com.tw>
2021-04-08 09:37:36 +0800

    ad

---
+++ 程式碼/0408/1.練習題-美金轉新台幣-介面/index.js
@@ -0,0 +1,9 @@
+// input
+const money = document.getElementById('money')
+
+// buttons
+const usd2twd = document.getElementById('usd2twd')
+const twd2usd = document.getElementById('twd2usd')
+
+// h1
+const moneyEx = document.getElementById('moneyEx')
