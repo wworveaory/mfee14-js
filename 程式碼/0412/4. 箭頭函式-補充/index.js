d6c8719bf5237ae0cd4ca0574e27f15a47258810
Anna <anna@joomla.com.tw>
2021-04-12 12:03:30 +0800

    Update index.js

--- 程式碼/0412/4. 箭頭函式-補充/index.js
+++ 程式碼/0412/4. 箭頭函式-補充/index.js
@@ -1,13 +1,21 @@
+// 傳統定義法(FD)
 function addOuter(a, b) {
   return function () {
     return a + b
   }
 }
 
+// 函式表達式(FE)
 const addOuter1 = function (a, b) {
   return function () {
     return a + b
   }
 }
 
+// 箭頭函式初步改寫
+const addOuter1 =  (a, b) => function () {
+    return a + b
+}
+
+// 箭頭函式最後改寫
 const addOuter2 = (a, b) => () => a + b
