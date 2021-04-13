49133ffbabff787101cc63623a1a4e370696bc23
Eddy Chang <eddy@joomla.com.tw>
2021-04-13 10:46:16 +0800

    a

---
+++ 程式碼/0413/5.猜拳遊戲-介面/index.js
@@ -0,0 +1,14 @@
+// 獲取元件介面物件
+// icon
+const scissors = document.getElementById('scissors')
+const paper = document.getElementById('paper')
+const rock = document.getElementById('rock')
+
+// h3
+const user = document.getElementById('user')
+const computer = document.getElementById('computer')
+const result = document.getElementById('result')
+
+scissors.addEventListener('click', function () {
+  alert('剪刀')
+})
